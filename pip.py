0113771f83df9c8e9c2e799030f4322ac0b62fdc
unknown <dalmq4806@gmail.comgit config --global user .name dalalgit config --global user.email dalmq4806@gmail.com>
2023-01-23 15:10:52 +0300

    lap.pip

---
+++ pip.py
@@ -0,0 +1,9 @@
+from art import*
+Art=text2art("BELIEVE and ACHEIVE",font='block',chr_ignore=True) # Return ASCII text with block font
+print(Art)
+
+Art=text2art("dalal") # random font mode
+print(Art)
+
+Art=text2art("Njoud") # Return ASCII text (default font) and default chr_ignore=True 
+print(Art)
