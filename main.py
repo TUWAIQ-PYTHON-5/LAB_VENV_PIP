fcf5dcf5132ba74286b7d016081141ec3b007be7
ZeNullOrb <elyas1999sb@hotmail.com>
2023-01-23 15:00:48 +0300

    VENV_PIP Lab

---
+++ main.py
@@ -0,0 +1,8 @@
+from art import *
+tprint(f"\nBELIEVE \nand \nACHEIVE\n\n",font="block",decoration="fancy3")
+
+tprint(f"\nReach\nfor\nthe\nstars\n\n",font="rand",decoration="arrow4")
+
+tprint(f"\nIf you\nwork\nit will\nwork\n\n",font="rnd-xlarge",decoration="cat3")
+
+tprint(f"\ndont\nwish\nfor it\nwork\nfor it\n\n",font="wizard",decoration="diamon3")
