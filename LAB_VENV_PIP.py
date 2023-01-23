c5fa7d94d2a3a7c0a54827bac61cfb96ff994d5f
HussamAltalib <hussamaltalib@outlook.com>
2023-01-23 14:51:11 +0300

    LAB_VENV_PIP

---
+++ LAB_VENV_PIP.py
@@ -0,0 +1,20 @@
+from art import *
+
+tprint("BELIEVE and ACHEIVE",font="block")
+
+tprint("Pyhton",font='block',chr_ignore=True)
+
+tprint("Camp",font="fancy5",decoration="barcode1")
+tprint("Camp",font="fancy5")
+
+tprint("Camp",font="fancy5",decoration="random")
+
+
+
+tprint("Camp",font="rand") #random
+
+
+
+
+
+
