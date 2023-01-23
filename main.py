1fee9c91642258afc14432d35cdc1f4cc349e367
nada.abdullah <none.4292@gmail.com>
2023-01-23 15:10:42 +0300

    lap are solved !

---
+++ main.py
@@ -0,0 +1,27 @@
+
+from art import *
+
+
+
+test = text2art("BELIEVE and ACHEIVE",font='block',chr_ignore=True) 
+print(test)
+
+
+
+
+user_input = input("write your name and we will make it pretty :D :")
+
+tprint(user_input,font="rnd-small")
+
+
+
+
+
+
+
+
+
+
+
+
+
