ca961c552ae114ebfb43d68e329cbbb96f272da6
NjoudAlhazmi <njoudz19@gmail.com>
2023-01-23 15:06:11 +0300

    solve the lap

---
+++ Text Art.py
@@ -0,0 +1,18 @@
+#designed with font block.
+from art import*
+
+Art=text2art("BELIEVE and ACHEIVE",font='block',chr_ignore=True)
+print(Art)
+
+
+#Bonus
+#1
+Art=text2art("Njoud")
+print(Art)
+#2
+Art=text2art("HELLO","rand")
+print(Art)
+#3
+tprint("PYTHON","rand")
+
+
