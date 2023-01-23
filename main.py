23430fa0da7a98ce9d336d6a5adaae97adf50ad5
Dooha005 <MINGW64@gmail.com~>
2023-01-23 15:06:32 +0300

    SOLVED LAB.

---
+++ main.py
@@ -0,0 +1,9 @@
+from art import *
+
+tprint("BELIEVE ",font="block",chr_ignore=True)
+
+
+# Come up with different phrases with different art and decoration.
+
+tprint("Dooha",font="cybermedum") # random font mode
+tprint("Nada",font="standard")
