72715c405ed70e160949b10442b152ef356afb18
asrar <asrar.rifae@hotmail.com>
2023-01-23 14:54:40 +0300

    solve LAB_VENV_PIP lab

---
+++ main.py
@@ -0,0 +1,13 @@
+from art import *
+from colorama import *
+
+tprint("BELIEVE and ACHEIVE")
+
+
+BELIEVE =text2art("BELIEVE ",font='block',chr_ignore=True) 
+andd =text2art(" and ",font='block',chr_ignore=True) 
+ACHEIVE =text2art("  ACHEIVE",font='block',chr_ignore=True) 
+
+print(Fore.BLUE + BELIEVE)
+print(Fore.RED + andd)
+print(Fore.BLUE + ACHEIVE)
