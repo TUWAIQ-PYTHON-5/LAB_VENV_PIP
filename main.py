27365c3a3ccc277fd95053554446798200309a4b
Yazeed <Yazeedrq9@gmail.com>
2023-01-23 15:04:56 +0300

    small edit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from art import *
 
-obArt=text2art("BELIEVE and ACHEIVE")
+obArt=text2art("HELLO WORLD")
+obArt1=text2art("BELIEVE and ACHEIVE")
+obArt2=text2art("NOPE")
 
 print(obArt)
+print(obArt1)
+print(obArt2)
