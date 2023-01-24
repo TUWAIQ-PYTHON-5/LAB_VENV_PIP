bc106a88c54d513e31e0b32b0186cd73d1b15b74
halrasheed <haroon.arrsheed@gmail.com>
2023-01-24 07:54:11 +0300

    PIP Lab upload

---
+++ VENV_PIP.py
@@ -0,0 +1,66 @@
+from art import *
+
+phrase :str = text2art("BELIEVE\n and\n ACHEIVE",font='block',chr_ignore=True)
+
+print(phrase)
+#tprint(phrase, font="block")
+print()
+print("_"*100)
+print()
+print(text2art("I Love Python",font='rand',chr_ignore=True))
+
+print()
+print("_"*100)
+print()
+print(text2art("Grow a Tree",font='rand',chr_ignore=True))
+
+print()
+print("_"*100)
+print()
+print(text2art("Plant Earth",font='rand',chr_ignore=True))
+
+print()
+print("_"*100)
+print()
+print(text2art("Programming Language",font='rand',chr_ignore=True))
+
+print()
+print("_"*100)
+print()
+print(text2art("Hungry man is Angry man",font='rand',chr_ignore=True))
+
+print()
+print("_"*100)
+print()
+print(text2art("East Park",font='rand',chr_ignore=True))
+
+print()
+print("_"*100)
+print()
+print(text2art("Art Works",font='rand',chr_ignore=True))
+
+print()
+print("_"*100)
+print()
+print(text2art("Saudi Arabia",font='rand',chr_ignore=True))
+
+print()
+print("_"*100)
+print()
+print(text2art("TWQ Academy",font='rand',chr_ignore=True))
+
+print()
+print("_"*100)
+print()
+print(text2art("Bootcamp",font='rand',chr_ignore=True))
+
+print()
+print("_"*100)
+print()
+print(text2art("Web Applications",font='rand',chr_ignore=True))
+
+print()
+print("_"*100)
+print()
+print(text2art("Homework",font='rand',chr_ignore=True))
+
